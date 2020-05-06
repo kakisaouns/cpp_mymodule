78e56c4e01dac86de4ab163a82fbbb782253521f
purasutiku1123@yahoo.co.jp <purasutiku1123@yahoo.co.jp>
2020-05-07 05:43:16 +0900

    add function "CopyFile"

--- fctrl.h
+++ fctrl.h
@@ -3,28 +3,45 @@
 
 #include <vector>
 #include <fstream>
+#include <filesystem>
 
 namespace Module{
     template<typename str_type>
-    bool ReadFile(std::vector<char> & data,const str_type & filepath){
+    bool ReadFile(std::vector<char> & result,const str_type & filepath){
         std::ifstream ifs(filepath);
         if(!ifs){return false;}
         
         ifs.seekg(0,std::ios_base::end);
-        data.resize(ifs.tellg());
+        result.resize(ifs.tellg());
         
         ifs.seekg(0,std::ios_base::beg);
-        ifs.read(&*data.begin(),data.size());
+        ifs.read(&*result.begin(),result.size());
 
         return true;
     }
 
     template<typename str_type>
-    bool WriteFile(const std::vector<char> & data,const str_type & str,std::ios_base::openmode mode = std::ios_base::binary){
-        std::ofstream ofs(str,mode);
+    bool WriteFile(const std::vector<char> & result,const str_type & filepath,std::ios_base::openmode mode = std::ios_base::binary){
+        std::ofstream ofs(filepath,mode);
         if(!ofs){return false;}
 
-        ofs.write(&*data.begin(),data.size());
+        ofs.write(&*result.begin(),result.size());
+        return true;
+    }
+
+    template<bool use_filesystem = true,typename dst_strtype,typename src_strtype>
+    bool CopyFile(const dst_strtype & dst,const src_strtype & src){
+        if constexpr(use_filesystem){
+            std::filesystem::copy_file(dst,src,std::filesystem::copy_options::overwrite_existing);
+        }else{
+            std::vector<char> dstdata;
+            if(!ReadFile(dstdata,dst)){
+                return false;
+            }
+            if(!WriteFile(dstdata,src)){
+                return false;
+            }
+        }
         return true;
     }
 
